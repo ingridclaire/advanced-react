dae428f2f7a78d192452a1aabe6e090dc4f3a9b4
Ingrid Carlson <ingridclaire@gmail.com>
2022-01-14 13:12:10 -0500

    adds styling to header and implements nprogress for quick route changes

--- sick-fits/frontend/pages/_document.js
+++ sick-fits/frontend/pages/_document.js
@@ -1,10 +1,22 @@
 import Document, { Html, Head, NextScript, Main } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage(
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      (App) => (props) => sheet.collectStyles(<App {...props} />)
+    );
+    const styleTags = sheet.getStyleElement();
+    // console.log(styleTags);
+    return { ...page, styleTags };
+  }
+
   render() {
     return (
       <Html lang="en">
-        {/* <Head></Head> */}
+        <Head />
         <body>
           <Main />
           <NextScript />
